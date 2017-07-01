6f747935dfb8db31f9bcecf41067ec62ac826f01
Gaurav Mehla <gaurav.ror007@gmail.com>
2017-07-01 09:43:55 +0530

    Add comments to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var uglify  = require('gulp-uglify');
 var watch = require('gulp-watch');
 var sourcemaps = require('gulp-sourcemaps');
 
+// Concat all JS files 
 gulp.task('scripts', function() {
   gulp.src(['./app/**/*.js', '!./app/**/*.test.js', '!./app/app.min.js'])
     .pipe(sourcemaps.init())
@@ -14,25 +15,27 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('public/javascripts/'));
 });
 
+// Concat all CSS files
 gulp.task('styles', function() {
   gulp.src(['./app/**/*.css', '!./app/app.min.css'])
       .pipe(concat('./app.min.css'))
       .pipe(gulp.dest('public/stylesheets/'));
 });
 
+// Copy index.html to /public/
 gulp.task('index', function() {
   gulp.src('app/index.html')
   .pipe(gulp.dest('public/'))
 });
 
-
+// Copy all templates to /public/templates/
 gulp.task('templates', function() {
   gulp.src('./app/templates/**/*')
   .pipe(gulp.dest('public/templates/'));
 });
 
 
-
+// Watch for change in css and js files
 gulp.task('watch', function() {
   watch(['./app/**/*.js', '!./app/**/*.test.js', '!./app/app.min.js'], function () {
     gulp.start('scripts');
@@ -43,4 +46,5 @@ gulp.task('watch', function() {
   });
 });
 
+// Default task
 gulp.task('default', ['scripts', 'styles', 'index', 'templates', 'watch']);

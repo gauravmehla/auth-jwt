4693772b40beafc06dd96e05430bd0ab012db803
Gaurav Mehla <gaurav.ror007@gmail.com>
2017-07-01 11:31:37 +0530

    Api configured

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+
+require('./api/models/db');
+require('./api/config/passport');
+
+var routesApi = require('./api/routes/index');
+
 var app = express();
 
 // view engine setup
@@ -18,8 +25,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize());
+app.use('/api', routesApi);
+
+
 app.use(function(req, res) {
   res.sendFile(path.join(__dirname, 'public/', 'index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
 });
 
 module.exports = app;
